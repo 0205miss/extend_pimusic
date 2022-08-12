421b8cf2499f4c14c0f05f8af0fca8a7d2ab92fe
eneaniagu <mmadusolo@gmail.com>
2022-08-12 08:46:19 +0000

    meuehe

--- pages/user/login.js
+++ pages/user/login.js
@@ -48,7 +48,7 @@ function Login(props) {
     const auth = async () => {
        
         const scopes = ['username', 'payments']
-        alert(scopes)
+        
         
         function onIncompletePaymentFound(payment) {
             var data = {
@@ -61,7 +61,7 @@ function Login(props) {
             return axios.post('https://nurbansports.com/pimus/public/pinetworkpay', data)
         }
          
-        Pi.authenticate(scopes, onIncompletePaymentFound).then(function (auth) {
+        await Pi.authenticate(scopes, onIncompletePaymentFound).then(function (auth) {
         
             setUserAuth(auth) 
             alert(auth.user.username)
